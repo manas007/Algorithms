6307ede6c7703158da704c8a393830329efd0381
mtripathi093@gmail.com <je19iyybs_>
2018-09-30 14:53:12 -0700

    Implementation added for binary search algo, and comments for understanding.

--- src/Sorting/BinarySearch.java
+++ src/Sorting/BinarySearch.java
@@ -6,26 +6,53 @@ import java.util.Random;
 public class BinarySearch {
 
     public static Random random  = new Random();
-    public static int arraySize = 28;
+    public static int arraySize = 28;  // number of elements you want
     public static int[] arrayToSearch = new int[arraySize];
-    public static int rangeMin = 20;
-    public static int rangeMax = 2000;
+    public static int rangeMin = 20;  // range of numbers that you want your array to have
+    public static int rangeMax = 2000; //
+    public static int numberToSearch=generateARandomNumberBetweenSpecificRange();
 
     public static void main(String[] args){
-        populateDataInArray();
-        Arrays.sort(arrayToSearch);
+        populateDataInArrayAndSortItForBinarySearch();
+        System.out.println("The values in array are : ");
         for(int i: arrayToSearch){
             System.out.println(i);
         }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("The Number to search is " + numberToSearch);
+        boolean result = runBinarySearchAlgo(); // actual method for binary serach implementation.
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Was the number found ? Answer : "+ result);
     }
 
-    public static void populateDataInArray(){
+    public static void populateDataInArrayAndSortItForBinarySearch(){
         for(int i=0; i<arrayToSearch.length; i++){
             arrayToSearch[i] = generateARandomNumberBetweenSpecificRange();
         }
+        Arrays.sort(arrayToSearch); // mandatory to have a sorted array for binary search
+
     }
 
     public static int generateARandomNumberBetweenSpecificRange(){
+        // random.nextInt(bound) will give a random number between 0 (inclusive) and the bound (exclusive)
+        // random.nextInt(30) will give any number between [0 .......... 29]
+        // so, random.nextInt(30) + 1 will give any number between [1 ...........30]
         return random.nextInt(rangeMax-rangeMin + 1) + rangeMin;
+    }
+
+    public static boolean runBinarySearchAlgo(){
+        int startIndex=0;
+        int lastIndex=arraySize-1;
+        // keep dividing the array to half, and update the startIndex and LastIndex of the broken array
+        while(lastIndex>startIndex) { // array keeps dividing itself unless there is only 1 element eft, or in other words, startIndex is same as lastIndex of array.
+            int middleIndex=(lastIndex + startIndex) / 2;
+            if(numberToSearch>arrayToSearch[middleIndex]){
+                startIndex=middleIndex+1;
+            }
+            else{
+                lastIndex=middleIndex;
+            }
+        }
+        return numberToSearch == arrayToSearch[lastIndex];
     }
 }
