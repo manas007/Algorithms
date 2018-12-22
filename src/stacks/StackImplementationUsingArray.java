9c1b50c897eafef07478973c7f6b8dd21e977339
mtripathi093@gmail.com <mtripathi093@gmail.com>
2018-12-22 13:20:57 -0800

    Implementation of Stack using Arrays

---
+++ src/stacks/StackImplementationUsingArray.java
@@ -0,0 +1,46 @@
+package stacks;
+
+public class StackImplementationUsingArray {
+
+    private String[] arr;
+    private int N = 0;
+
+    public StackImplementationUsingArray(int capacity){
+        arr = new String[capacity];
+    }
+
+    public void push(String item){
+        arr[N++] = item; // first put the item at N and then increase N by 1;
+    }
+
+    public String pop(){
+        return arr[--N];
+    }
+
+    public boolean isEmpty(){
+        return N == 0;
+    }
+
+    public boolean isFull(){
+        return N == arr.length;
+    }
+
+
+    public static void main(String[] args){
+        StackImplementationUsingArray testObj = new StackImplementationUsingArray(4);
+        testObj.push("a");
+        testObj.push("b");
+        testObj.push("c");
+        testObj.push("d");
+        System.out.println("Stack is now Full ? " + testObj.isFull());
+
+        System.out.println(testObj.pop());
+        System.out.println(testObj.pop());
+        System.out.println(testObj.pop());
+        System.out.println(testObj.pop());
+        System.out.println("Stack is now empty ? " + testObj.isEmpty());
+
+    }
+}
+
+
